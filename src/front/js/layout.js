70e23248976838033434752c8e7d13557b3e1d2d
billferrabone <billferrabone@gmail.com>
2023-02-09 01:16:24 +0000

    Mi perfil de usuario

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,6 +4,7 @@ import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
+import { Userprofile } from "./pages/userprofile";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
@@ -23,6 +24,7 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<Userprofile />} path="/userprofile" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
