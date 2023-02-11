7a5f78b322c7bc844942808a0d0030db6281ecc5
Ndavlo <nicolasdl99@gmail.com>
2023-02-11 02:52:24 +0000

    login front v1

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">TrackBit</span>
 				</Link>
 				<div className="ml-auto" id="navbarButtons">
-				<button id="navButton" type="button" className="btn btn-sm">Newsletter</button>
+				<button id="navButton" type="button" className="btn btn-sm">Blog</button>
 				<button id="navButton" type="button" className="btn btn-sm">Registrate</button>
 				<button id="navButton" type="button" className="btn btn-sm">Log In</button>
 				</div>
---
+++ src/front/js/pages/login.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import "../../styles/login.css";
+
+import { Context } from "../store/appContext";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<>
+			<div id="loginContainer">
+				<div className="loginForm">
+					<form>
+						<div class="mb-3">
+							<label for="exampleInputEmail1" class="form-label">Email address</label>
+							<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+								<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+						</div>
+						<div class="mb-3">
+							<label for="exampleInputPassword1" class="form-label">Password</label>
+							<input type="password" class="form-control" id="exampleInputPassword1"/>
+						</div>
+						<div class="mb-3 form-check">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+								<label class="form-check-label" for="exampleCheck1">Check me out</label>
+						</div>
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</form>
+
+				</div>
+
+
+				<div className="loginImg"></div>
+			</div>
+		</>
+	);
+};
