046e3a8995a1edaec4e73619c59b1ab4eccad948
Ndavlo <nicolasdl99@gmail.com>
2023-02-13 01:39:36 +0000

    mejorando login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -10,24 +10,23 @@ export const Login = () => {
 	return (
 		<>
 			<div id="loginContainer">
-				<div className="loginForm">
+				<div className="loginFormCont">
 					<form id="formLogin">
+					<h1 id="loginHead">Accede a tu cuenta</h1>
 						<div class="mb-3">
-							<label for="exampleInputEmail1" class="form-label">Email address</label>
+							<label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
 							<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-								<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 						</div>
 						<div class="mb-3">
-							<label for="exampleInputPassword1" class="form-label">Password</label>
+							<label for="exampleInputPassword1" class="form-label">Contraseña</label>
 							<input type="password" class="form-control" id="exampleInputPassword1"/>
 						</div>
 						<div class="mb-3 form-check">
 							<input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-								<label class="form-check-label" for="exampleCheck1">Check me out</label>
+								<label class="form-check-label" for="exampleCheck1">Mantener sesion inciada</label>
 						</div>
-						<button type="submit" class="btn btn-primary">Submit</button>
+						<button type="submit" class="btn btn-primary">Acceder</button>
 					</form>
-
 				</div>
 
 
