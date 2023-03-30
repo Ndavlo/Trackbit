788fa0ead28cb26902bb38ba0b4bbfc1d926e86f
Ndavlo <nicolasdl99@gmail.com>
2023-03-30 01:23:30 +0000

    Render

--- render_build.sh
+++ render_build.sh
@@ -6,5 +6,12 @@ npm install
 npm run build
 
 pipenv install
+# Si hay archivos de claves se deben copiar desde la carpeta /etc/secrets
+# a esta carpeta se suben los archivos desde la configuracion del entorno
+cp /etc/secrets/firebase_credentials.json ./firebase_credentials.json 
 
-pipenv run upgrade
+
+rm -R -f ./migrations && 
+pipenv run init && 
+pipenv run migrate && 
+pipenv run upgrade 
